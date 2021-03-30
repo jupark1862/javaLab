72c3dc6d103566f88f469e50148da9c7879775ed
user <user@121.168.62.71>
2021-03-30 11:36:50 +0900

    Chap11 예제

---
+++ Chap11Lab/src/sec06/exam01_class/ClassExample.java
@@ -0,0 +1,24 @@
+package sec06.exam01_class;
+
+public class ClassExample {
+
+	public static void main(String[] args) {
+		Car car = new Car();
+		Class clazz1 = car.getClass();		//°´Ã¼.getClass()·Î ÇØ´ç °´Ã¼ÀÇ Class °´Ã¼¸¦ ¾ò´Â´Ù
+		System.out.println(clazz1.getName());
+		System.out.println(clazz1.getSimpleName());
+		System.out.println(clazz1.getPackage());
+		System.out.println();
+		
+		try {
+			// Class.forName()·Î ÇØ´ç °´Ã¼ÀÇ Class °´Ã¼¸¦ ¾ò´Â´Ù
+			Class clazz2 = Class.forName("sec06.exam01_class.Car");
+			System.out.println(clazz2.getName());
+			System.out.println(clazz2.getSimpleName());
+			System.out.println(clazz2.getPackage());
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
