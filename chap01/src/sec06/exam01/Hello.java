286ef795828f0ff1e4fd28c917c7b57fe041a68b
jupark1862 <kolpks0@naver.com>
2021-04-01 10:38:25 +0900

    Chap01 예제

---
+++ chap01/src/sec06/exam01/Hello.java
@@ -0,0 +1,12 @@
+package sec06.exam01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("1.이름 : 박주형");
+			System.out.println("2.전화번호 : 010-5560-6962");
+			System.out.println("3.학습목표 : 취업");
+	}
+
+}
