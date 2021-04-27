2be849fc9a92f74c2230226ba4346b94e2c015a2
jupark1862 <kolpks0@naver.com>
2021-04-27 16:10:04 +0900

    chap12 예제

---
+++ chap12Lab/src/sec02/exam01_ccreatetjread/BeepPrintExample.java
@@ -0,0 +1,15 @@
+package sec02.exam01_ccreatetjread;
+
+import java.awt.Toolkit;
+
+public class BeepPrintExample {
+
+	public static void main(String[] args) {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		for(int i=0; i<5; i++) {
+			toolkit.beep();
+			try {Tjread.sleep(1000);}catch(example)
+		}
+	}
+
+}
---
+++ chap12Lab/src/sec02/exam01_ccreatetjread/BeepPrintExample2.java
@@ -0,0 +1,17 @@
+package sec02.exam01_ccreatetjread;
+
+public class BeepPrintExample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		thread.start();
+		
+		for(int i=0; i<5; i++) {
+			System.out.println("¶ò");
+			try {Thread.sleep(millis);
+		}
+
+	}
+
+}
